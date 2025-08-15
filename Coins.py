9d2fd639554cd91a076553a2da9c094e117a0d9d
NATHAN <nathan.handling@gmail.com>
2025-08-14 21:52:59 -0700

    Small set back but this rewritten to work for zyBook lab! Stupid return value!

--- Coins.py
+++ Coins.py
@@ -1,71 +1,57 @@
-def exact_change():
+def exact_change(user_input):
 
-    x = input("Please Enter Change Amount \n")
-    
-    #converting str -> float -> int to get expected output. 
-    #I am doing this check because some users may input their change 
-    #in the traditional format but my code expects a single large int
-
-    if "." in x:
-        x = float(x)
-        x = int(x * 100)
-    else:
-        x = int(x)
-
-    if x == 0:
+    if user_input <= 0:
         print("no change")
-        return 
-    
-    #dollar check
 
-    num_dollars = x // 100
-    remainder = x % 100
+    num_dollars = 0
+    num_quarters = 0
+    num_dimes = 0
+    num_nickels = 0
+    num_pennies = 0
+
+    num_dollars = user_input // 100
+    if num_dollars > 0 and user_input > 0:
+        user_input = user_input - (num_dollars * 100)
     if num_dollars == 1:
         print(num_dollars, "dollar")
     elif num_dollars > 1:
         print(num_dollars, "dollars")
     
-    #quarter check
-
-    if remainder > 24: 
-        num_quarters = remainder // 25
-        remainder = remainder % 25
-        if num_quarters == 1:
-            print(num_quarters, "quarter")
-        elif num_quarters > 1:
-            print(num_quarters, "quarters")
+    num_quarters = user_input // 25
+    if num_quarters > 0 and user_input > 0:
+        user_input = user_input - (num_quarters * 25)
+    if num_quarters == 1:
+        print(num_quarters, "quarter")
+    elif num_quarters > 1:
+        print(num_quarters, "quarters")
     
-    #dime check
-
-    if remainder > 9: 
-        num_dimes = remainder // 10
-        remainder = remainder % 10
-        if num_dimes == 1:
-            print(num_dimes, "dime")
-        elif num_dimes > 1:
-            print(num_dimes, "dimes")
-
-    #nickle check
-
-    if remainder > 4:
-        num_nickles = remainder // 5
-        remainder = remainder % 5
-        if num_nickles == 1:
-            print(num_nickles, "nickle")
-        elif num_nickles > 1:
-            print(num_nickles, "nickles")
-
-    #penny check
-
-    if remainder > 0:
-        num_pennies = remainder // 1
-        if num_pennies == 1:
-            print(num_pennies, "penny")
-        elif num_pennies > 1:
-            print(num_pennies, "pennies")
+    num_dimes = user_input // 10
+    if num_dimes > 0 and user_input > 0:
+        user_input = user_input - (num_dimes * 10)
+    if num_dimes == 1:
+        print(num_dimes, "dime")
+    elif num_dimes > 1:
+        print(num_dimes, "dimes")
     
-#Final Check
+    num_nickels = user_input // 5
+    if num_nickels > 0 and user_input > 0:
+        user_input = user_input - (num_nickels * 5)
+    if num_nickels == 1:
+        print(num_nickels, "nickel")
+    elif num_nickels > 1:
+        print(num_nickels, "nickels")
 
-if __name__=="__main__":
+    num_pennies = user_input // 1
+    if num_pennies > 0 and user_input > 0:
+        user_input = user_input - (num_pennies * 1)
+    if num_pennies == 1:
+        print(num_pennies, "penny")
+    elif num_pennies > 1:
+        print(num_pennies, "pennies")
 
-    exact_change()
+    return(num_dollars, num_quarters, num_dimes, num_nickels, num_pennies)
+
+if __name__ == "__main__":
+
+    user_total = int(input())
+    exact_change(user_total)
