f9982c66f717f9745e2654cba0f6d9733912a446
Nathan Handling <nathan.handling@gmail.com>
2025-08-14 18:02:13 -0700

    Added input ability and conversion within function

--- Coins.py
+++ Coins.py
@@ -1,6 +1,19 @@
-def exact_change(x):
+def exact_change():
+
+    x = input("Please Enter Change Amount \n")
+    
+    #converting str -> float -> int to get expected output. 
+    #I am doing this check because some users may input their change 
+    #in the traditional format but my code expects a single large int
+
+    if "." in x:
+        x = float(x)
+        x = int(x * 100)
+    else:
+        x = int(x)
     
     #dollar check
+
     num_dollars = x // 100
     remainder = x % 100
     if num_dollars == 1:
@@ -9,6 +22,7 @@ def exact_change(x):
         print(num_dollars, "dollars")
     
     #quarter check
+
     if remainder > 24: 
         num_quarters = remainder // 25
         remainder = remainder % 25
@@ -18,6 +32,7 @@ def exact_change(x):
             print(num_quarters, "quarters")
     
     #dime check
+
     if remainder > 9: 
         num_dimes = remainder // 10
         remainder = remainder % 10
@@ -27,6 +42,7 @@ def exact_change(x):
             print(num_dimes, "dimes")
 
     #nickle check
+
     if remainder > 4:
         num_nickles = remainder // 5
         remainder = remainder % 5
@@ -36,6 +52,7 @@ def exact_change(x):
             print(num_nickles, "nickles")
 
     #penny check
+
     if remainder > 0:
         num_pennies = remainder // 1
         if num_pennies == 1:
@@ -44,7 +61,7 @@ def exact_change(x):
             print(num_pennies, "pennies")
     
 #Final Check
+
 if __name__=="__main__":
 
-    x = 9999
-    exact_change(x)
+    exact_change()
